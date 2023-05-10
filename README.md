d1609c5ee63da0ba7e437efb1f43649ba5542eef
derricn2 <derricn2@gmail.com>
2023-05-09 20:48:11 -0700

    updated/added link/sources to README, updated box shadow to a more fitting color, updated choice box width to register entire button when clicked, added padding to so choice boxes are more form fitted to the page, added a lot of functionality to game (noted and defined JS code), added end HTML page for when all questions are answered

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # JavaScript-Timed-Quiz
 
 Bootcamp Challenge 04 - Web API's: A timed quiz on coding
+
+
+
+## Sites
+
+- [appendChild()](https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild)
+- [addEventListener()](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
+- [Building Blocks](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events#event_delegation)
+- [createElement()](https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+- [Data Attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes)
+- [Document](https://developer.mozilla.org/en-US/docs/Web/API/Document)
+- [Events](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events)
+- [event:preventDefault()](https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)
+- [JSON.stringify()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify)
+- [KeyboardEvent](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
+- [localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
+- [querySelectorAll()](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll)
+- [setAttribute()](https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute)
+- [setInterval()](https://developer.mozilla.org/en-US/docs/Web/API/setInterval)
+- [Style Property](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style)
+- [Web Storage API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API)
+- [Window](https://developer.mozilla.org/en-US/docs/Web/API/Window)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,113 @@
 // pulling questions and choices from html
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+var question = document.getElementById("question");
+var choices = Array.from(document.getElementsByClassName('choice-text'));
 
+var currentQuestion = {};
+var acceptingAnswers = false;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        answer: 3
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _____.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parentheses",
+        choice4: "square brackets",
+        answer: 3
+    },
+    {
+        question: "Arrays in JavaScript can be used to store_____.",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: 4
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parentheses",
+        answer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "terminal / bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        answer: 4
+    },
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        choice1: "<javascript>",
+        choice2: "<script>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 2
+    },
+];
+
+// value of getting question correct
+const CORRECT_BONUS = 5;
+// amount of total questions
+const MAX_QUESTIONS = 6;
+
+// start quiz 
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+// pulls different question from var questions array
+getNewQuestion = () => {
+
+        // once all questions are answered, end webpage will be loaded
+        if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+            return window.location.assign("./end.html");
+        }
+    questionCounter++;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    // brings the appropriate answer choices that go along with the appropriate question, prevents different answer choices to mix with other questions
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    // prevents asking repeat questions
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+// after choice is clicked, a new question is loaded
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false; 
+        var selectedChoice = e.target;
+        var selectedAnswer = selectedChoice.dataset["number"];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
+
+startGame();
