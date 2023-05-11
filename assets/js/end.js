ef786bb8bdde94e7a55011587548c3575db7583c
derricn2 <derricn2@gmail.com>
2023-05-11 16:58:07 -0700

    troubleshoot JS to properly load homescreen after saving high score

--- assets/js/end.js
+++ assets/js/end.js
@@ -36,7 +36,7 @@ saveHighScore = (e) => {
 
     // after inputting score, brings you back to homepage
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('./index.html');
 
     console.log(highScores);
 };
